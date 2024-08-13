f27e138a4c5e04f956ba1d88e8a794d762ca654d
Mr.MF <Mr.MF@foxmail.com>
2024-08-13 09:44:19 +0800

    chore(style): update code style configuration

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+export default {
+  tabWidth: 2,
+  semi: true,
+  printWidth: 108,
+  singleQuote: false,
+  endOfLine: "lf",
+  quoteProps: "consistent",
+  htmlWhitespaceSensitivity: "strict",
+  vueIndentScriptAndStyle: true,
+};
