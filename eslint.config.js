33a8a979a483863055e786f5faf34db15515b2da
Mr.MF <Mr.MF@foxmail.com>
2024-08-13 16:15:35 +0800

    chore(deps): update configuration

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslint_ts from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["**/node_modules", "**/dist", "**/.cache", "**/.idea", "**/*.d.ts"],
+  },
+  eslint.configs.recommended,
+  ...eslint_ts.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    rules: { "@typescript-eslint/no-explicit-any": "off" },
+  },
+];
