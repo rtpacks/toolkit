51a1fce655bfdb33907a924417ffc16f3d7664a9
Mr.MF <Mr.MF@foxmail.com>
2024-08-16 09:40:35 +0800

    feat(vue): export hooks

--- packages/vue/index.ts
+++ packages/vue/index.ts
@@ -1,3 +1,6 @@
 export * from "./src/use-ext-router/index";
 export * from "./src/use-replace-state/index";
 export * from "./src/use-route-listener/index";
+export * from "./src/use-select";
+export * from "./src/use-switchable";
+export * from "./src/use-tree";
