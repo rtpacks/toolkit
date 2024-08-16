a0c8062b4113979126963121f7a16726ed01a002
Mr.MF <Mr.MF@foxmail.com>
2024-08-16 10:51:02 +0800

    chore(build): update the release configuration

---
+++ scripts/release.ts
@@ -0,0 +1,17 @@
+import process from "node:process";
+import { execSync } from "node:child_process";
+import fs from "fs-extra";
+// import { readJSONSync } from "fs-extra"; // error, The requested module 'fs-extra' does not provide an export named 'readJSONSync'
+
+const { version: oldVersion } = fs.readJSONSync("package.json");
+execSync("bumpp -r --no-commit --no-tag --no-push", { stdio: "inherit" });
+const { version } = fs.readJSONSync("package.json");
+
+if (oldVersion === version) {
+  console.log("canceled");
+  process.exit();
+}
+
+execSync("git add .", { stdio: "inherit" });
+execSync(`git commit -m "chore: release v${version}"`, { stdio: "inherit" });
+execSync(`git tag -a v${version} -m "v${version}"`, { stdio: "inherit" });
