b47580b1c9b7a9b91c585c4180a873671c6cda6b
Mr.MF <Mr.MF@foxmail.com>
2024-08-20 17:17:07 +0800

    docs(core): add TSDoc comments to useSpliter function

--- packages/core/src/use-spliter/index.ts
+++ packages/core/src/use-spliter/index.ts
@@ -3,9 +3,39 @@ import type { Unit } from "../utils";
 
 export type TransFormFn<T extends Unit = Unit> = (v: string, index: number, arr: string[]) => T;
 
+/**
+ * A custom hook to manage string splitting and joining using a delimiter.
+ *
+ * @param spliter - The delimiter used for string operations. Default is ":".
+ *
+ * @returns An object containing the following methods:
+ * - splice(prev: Unit, next: Unit): Joins two units with the specified delimiter.
+ * - split<T extends Unit = Unit>(v: string, transform?: TransFormFn<T>): T[] | never: Splits a string into an array of units using the delimiter. Applies an optional transformation function.
+ * - take(v: string): Retrieves the first element from the split result of the given string.
+ *
+ * @example
+ * ```typescript
+ * import useSpliter from '@rtpacks/core';
+ *
+ * const toNumber = (value: string) => parseInt(value, 10);
+ * const { splice, split, take } = useSpliter();
+ *
+ * const joinedString = splice('apple', 'banana');
+ * console.log(joinedString); // Output: "apple:banana"
+ *
+ * // Example of using split
+ * const str = "1:2";
+ * const splitResult = split<number>(str, toNumber);
+ * console.log(splitResult); // Output: [1, 2]
+ *
+ * // Example of using take
+ * const firstElement = take("orange:grape");
+ * console.log(firstElement); // Output: "orange"
+ * ```
+ */
 export default function useSpliter(spliter = ":") {
   /**
-   * 拼接方法
+   * Joins two units with the specified delimiter.
    * @param prev
    * @param next
    * @returns
@@ -15,8 +45,8 @@ export default function useSpliter(spliter = ":") {
   };
 
   /**
-   * 分割方法
-   * @param v
+   * Splits a string into an array of units using the delimiter. Applies an optional transformation function.
+   * @param v - String to be split.
    * @param transform
    * @returns
    */
@@ -26,13 +56,13 @@ export default function useSpliter(spliter = ":") {
 
       return isFunction(transform) ? res.map(transform) : (res as T[]);
     }
-    throw new Error("解析失败，无效的分隔符");
+    throw new Error("Parsing failed, invalid delimiter.");
   };
 
   /**
-   * 获取首个元素
-   * @param v
-   * @returns
+   * Retrieves the first element from the split result of the given string.
+   * @param v - String to be split.
+   * @returns The first element
    */
   const take = (v: string) => {
     return split<string>(v)[0];
