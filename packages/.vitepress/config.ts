1672bef6a4a2c1ceb433cecc69667698d178b61e
Mr.MF <Mr.MF@foxmail.com>
2024-08-27 17:59:43 +0800

    chore(vitepress): update vitepress configuration

--- packages/.vitepress/config.ts
+++ packages/.vitepress/config.ts
@@ -1,4 +1,11 @@
 import { defineConfig } from "vitepress";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+import VueJsx from "@vitejs/plugin-vue-jsx";
+import { version } from "../../package.json";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const root = resolve(__dirname, `../`);
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -6,17 +13,29 @@ export default defineConfig({
   description: "The useful tools",
   lang: "zh-CN",
   cleanUrls: true,
+  lastUpdated: true,
 
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: "Home", link: "/" },
-      { text: "Examples", link: "/markdown-examples" },
+      {
+        text: `v${version}`,
+        items: [{ text: "更新日志", link: "/docs/CHANGELOG.md" }],
+      },
     ],
+    search: { provider: "local" },
 
     sidebar: [
       {
-        text: "Examples",
+        text: "@rtpackx/core",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
+      {
+        text: "@rtpackx/vue",
         items: [
           { text: "Markdown Examples", link: "/markdown-examples" },
           { text: "Runtime API Examples", link: "/api-examples" },
@@ -24,6 +43,21 @@ export default defineConfig({
       },
     ],
 
-    socialLinks: [{ icon: "github", link: "https://github.com/vuejs/vitepress" }],
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright (c) 2024-present, Mr.MF",
+    },
+
+    socialLinks: [{ icon: "github", link: "https://github.com/rtpacks/toolkit" }],
+  },
+
+  vite: {
+    resolve: {
+      alias: [{ find: "@", replacement: resolve(__dirname, "../") }],
+    },
+    plugins: [
+      VueJsx(),
+      // MarkdownTransform(),
+    ],
   },
 });
