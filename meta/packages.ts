fa75e1eac025db091338e61d79ab80fb12796ff8
Mr.MF <Mr.MF@foxmail.com>
2024-08-29 11:49:13 +0800

    feat(vitepress): add type declaration

--- meta/packages.ts
+++ meta/packages.ts
@@ -1,30 +1,4 @@
-export interface PackageManifest {
-  name: string;
-  entry: string;
-  outDir: string;
-  outputFileName?: string;
-  /**
-   * 是否使用 Vue 及相关插件
-   */
-  useVue?: boolean;
-
-  /* feature 额外的拓展 */
-  author?: string;
-  description?: string;
-  external?: string[];
-  globals?: Record<string, string>;
-  manualImport?: boolean;
-  deprecated?: boolean;
-  submodules?: boolean;
-  build?: boolean;
-  iife?: boolean;
-  cjs?: boolean;
-  mjs?: boolean;
-  dts?: boolean;
-  target?: string;
-  utils?: boolean;
-  copy?: string[];
-}
+import { PackageManifest } from "./types";
 
 export const packages: PackageManifest[] = [
   // @rtpackx/core
