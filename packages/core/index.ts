42f48c03490936d5e39db1ac39df81dcbad649cc
Mr.MF <Mr.MF@foxmail.com>
2025-11-24 15:25:00 +0800

    chore(core): update config

--- packages/core/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./src/monitor/index";
-export * from "./src/indexdb/index";
-export * from "./src/use-spliter/index";
-export * from "./src/utils/index";
---
+++ packages/core/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./indexdb/index";
+export * from "./monitor/index";
+export * from "./use-cache/index";
+export * from "./use-spliter/index";
+export * from "./utils/index";
