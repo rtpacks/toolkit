72628c4f00c8024e9b9156777ab69fe75ad76522
Mr.MF <Mr.MF@foxmail.com>
2025-11-24 16:22:34 +0800

    fix(vite): remove compress plugin

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -1,3 +1,3 @@
-export * from "./plugins/compress";
+// export * from "./plugins/compress"; build error
 export * from "./plugins/lazyload-normalize";
 export * from "./plugins/staticload-normalize";
